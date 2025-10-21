dfd0354eb34717dcdec3fe533271e8dd90c0673f
LuisFuentes90 <lefe_90@hotmail.com>
2025-10-20 19:50:50 -0600

    Add files via upload

---
+++ NotasApp/src/notas/Estudiante.java
@@ -0,0 +1,16 @@
+package notas;
+
+public class Estudiante {
+
+    public String nombre;
+    public double guia;
+    public double parcial;
+    public double promedio;
+
+    public Estudiante(String nombre, double guia, double parcial) {
+        this.nombre = nombre;
+        this.guia = guia;
+        this.parcial = parcial;
+        this.promedio = Math.round((guia * 0.4 + parcial * 0.6) * 10.0) / 10.0;
+    }
+}
